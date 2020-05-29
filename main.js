ace580425bcae500b22eed2d887fe8624a9ef40f
Michal Orzechowski <Corvete600@poczta.fm>
2020-05-29 21:28:09 +0200

    new things added

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const name = "Michal";
 const age = 32;
 
-'alert("Jesteś na stronie Michała, Cześć");
-'console.log(`Mam na imie ${name} i mam ${age} lata`);
-
-const hi = document.querySelector('.heading-contact--js');
-hi.innerHTML ='Witam na stornie kontaktowej. Chcesz uzyskać kontakt, zrób to za pomocą formularza kontaktowego'
+const hi = document.querySelector('.heading-contact--js, .head-title__js');
+hi.innerHTML=`Cześć, chcesz się ze mną skontaktować, użyj formularza kontaktowego`
+const about = document.querySelector('.head-title__js')
+about.innerHTML=`czesc, mam na imię ${name} i mam ${age} lata`
